270b4fa115548bdf657f373c990a78a82233e6c3
Qiu233 <lopi258@outlook.com>
2026-08-03 13:57:44 +0800

    ci: automate tagged releases

--- lakefile.lean
+++ lakefile.lean
@@ -3,6 +3,8 @@ open Lake DSL
 
 package "binary" where
   version := v!"0.1.0"
+  releaseRepo := "https://github.com/Lean-zh/binary"
+  preferReleaseBuild := true
 
 @[default_target]
 lean_lib Binary where
